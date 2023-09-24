4e51fbc7e769e2c6487bf33224f7ee4781cec58d
Ilyas <studiumilyas@gmail.com>
2023-09-24 20:11:29 +0200

    NLP and PCA

--- feature_engineering_transformers.py
+++ feature_engineering_transformers.py
@@ -9,6 +9,9 @@ import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
 from nltk.corpus import stopwords
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
 
 def is_user_creator(biography, keywords):
     # Tokenize the biography and convert to lower case
@@ -22,7 +25,7 @@ def is_user_creator(biography, keywords):
 
 
 # Define a string match transformer class
-class StringMatchTransformer(BaseEstimator, TransformerMixin):
+class BiographyTransformer(BaseEstimator, TransformerMixin):
     # Initialize the class with a list of keywords
     def __init__(self, keywords):
         self.keywords = keywords
@@ -36,3 +39,35 @@ class StringMatchTransformer(BaseEstimator, TransformerMixin):
         X = pd.DataFrame(X)
         results = X[0].apply(lambda biography: is_user_creator(biography, self.keywords))
         return results.to_numpy().reshape(-1,1)
+
+
+# Define a PCA transformer class
+class PCATransformer(BaseEstimator, TransformerMixin):
+    # Initialize the class with a standardscaler and pca
+    def __init__(self):
+        self.standard_scaler = StandardScaler()
+        self.pca = PCA()
+
+    # Fit method
+    def fit(self, X, y=None):
+        self.pca.fit(self.standard_scaler.fit_transform(X))
+        return self
+
+    # Transform method to return the principal components 
+    def transform(self, X):
+        return self.pca.transform(self.standard_scaler.transform(X))
+
+
+# Define a missing value identifier class
+class MissingValueIdentifier(BaseEstimator, TransformerMixin):
+    # Initialize the class with a feature name
+    def __init__(self, feature_name):
+        self.feature_name = feature_name
+
+    # Fit method for compatibility with sklearn API
+    def fit(self, X, y=None):
+        return self
+
+    # Transform method to check if the column values are null  
+    def transform(self, X):
+        return X[self.feature_name].isna().to_numpy().reshape(-1, 1)
